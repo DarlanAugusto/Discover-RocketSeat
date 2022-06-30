d816417f5523b00f06da0f16ddd0fcfc149acc8a
Darlan Silva <darlansilv4@gmail.com>
2022-06-29 21:53:05 -0300

    Outra maneira de entender funções

---
+++ Guia Estelar JavaScript/Functions/functions_3.js
@@ -0,0 +1,10 @@
+// Funcção é um liquidificador
+
+
+function fazerSuco(fruta1, fruta2) {
+    return  `Suco de ${fruta1} com ${fruta2}`;
+}
+
+const copo = fazerSuco("banana", "maçã");
+
+console.log(copo);
