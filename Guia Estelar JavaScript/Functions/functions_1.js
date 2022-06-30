080982946842e1f2e7ff885e825f2a43eb1b57b3
Darlan Silva <darlansilv4@gmail.com>
2022-06-29 21:39:10 -0300

    Argumentos e parâmetros

---
+++ Guia Estelar JavaScript/Functions/functions_1.js
@@ -0,0 +1,9 @@
+// function expression
+// function anonymous
+
+// paramentros (parameters)
+const sum = function (number1, number2) {
+    console.log(number1 + number2);
+}
+
+sum(2, 3); // arguments - argumentos
