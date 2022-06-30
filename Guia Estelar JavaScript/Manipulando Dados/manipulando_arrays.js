71576b4ce785c5d94af8cfcc5fae67ad4de04875
Darlan Silva <darlansilv4@gmail.com>
2022-06-29 23:36:56 -0300

    Manipulando Arrays

---
+++ Guia Estelar JavaScript/Manipulando Dados/manipulando_arrays.js
@@ -0,0 +1,9 @@
+// Manipulando Arrays
+
+
+let word = "paralelepípedo";
+
+console.log("Palavra:", word);
+console.log("Array da palavra:", Array.from(word));
+console.log("Palavra separada por traços:", Array.from(word).join("-"));
+console.log("Palavra separada por traços, em caixa alta:", Array.from(word).join("-").toUpperCase());
