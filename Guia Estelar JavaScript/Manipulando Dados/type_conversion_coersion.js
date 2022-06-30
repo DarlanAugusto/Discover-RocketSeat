97938ced9d17d1f09def8bd1cf6f954134732db8
Darlan Silva <darlansilv4@gmail.com>
2022-06-29 23:10:06 -0300

    Type conversion X Type coersion

---
+++ Guia Estelar JavaScript/Manipulando Dados/type_conversion_coersion.js
@@ -0,0 +1,10 @@
+/* 
+    Type conversion (typecasting) vs Type coersion
+    
+    - é uma alteração no tipo do dado
+    - conversion: conversão realizada pelo dev
+    - coersion: conversão realizada pelo JS automaticamente
+
+*/
+
+console.log(Number("9") + 5);
