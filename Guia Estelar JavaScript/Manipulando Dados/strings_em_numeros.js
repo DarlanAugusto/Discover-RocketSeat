e141012c820b7e9ba0b3f487db57ebd313bfc57c
Darlan Silva <darlansilv4@gmail.com>
2022-06-29 23:13:17 -0300

    Manipulando Strings e Números

---
+++ Guia Estelar JavaScript/Manipulando Dados/strings_em_numeros.js
@@ -0,0 +1,11 @@
+// Manipulando Strings e Números
+
+// Transformando string em numeró e vice-versa
+
+let string = "123";
+
+console.log(Number(string));
+
+let number = 321;
+
+console.log(String(number));
